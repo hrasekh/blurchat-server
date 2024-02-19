2cc827bb9409db8dee810c4414472b87ffa4b2f4
Hadi Rasekh <hadi_rasekh@yahoo.com>
2024-02-19 21:47:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const { createServer } = require('node:http');
+const app = express();
+const server = createServer(app);
+
+app.get("/", (req, res) => {
+    res.send("Server is running ... ")
+});
+
+const PORT = process.env.PORT || 5678;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT || 5678}`);
+    console.log(`http://localhost:${process.env.PORT || 5678}`);
+});
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        method: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log("A user connected", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected", socket.id);
+        // socket.broadcast.emit("user-disconnected", socket.id);
+    });
+
+    socket.on("call-user", (data) => {
+        console.log("recieved a call-user:", socket.id, "to :", data.to);
+        io.to(data.to).emit("call-mode", {
+            from: socket.id,
+            signalData: data.signalData,
+        });
+    });
+
+    socket.on("call-answer", (data) => {
+        io.to(data.to).emit("answer-mode", {
+            // from: socket.id,
+            signalData: data.signalData,
+        });
+    });
+});
